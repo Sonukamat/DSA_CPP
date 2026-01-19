bdbc80a8ecff909cdf4bee1b4b0e86ab675bdefe
Sonukamat <sonukamatagc192@gmail.com>
2026-01-19 22:34:15 +0530

    first commit

---
+++ if_else2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a,b;
+  cout<<"enter your 1st no:";
+  cin>>a;
+  cout<<"enter your 2nd no:";
+  cin>>b;
+  if(a>b) {
+    cout<<"yes";
+  }
+  else{
+    cout<<"no";
+    
+  }
+
+
+}
