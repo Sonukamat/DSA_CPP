548aca124b5f43d1f6c69119fcd3d28800cbb925
Sonukamat <sonukamatagc192@gmail.com>
2026-01-19 23:00:52 +0530

    Add loop example in loop.cpp to print "coder army" five times

---
+++ 2nd.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a,b;
+  cout<<"enter your 1st no";
+  cin>>a;
+  cout<<"enter your 2nd number";
+  cin>>b;
+  cout<<a+b;
+  return 0;
+}
